e590d5a9edb64efa4ec9dc953723469bb04116ba
dayojohn19 <dayo_john16@yahoo.com>
2025-03-24 08:21:08 +0800

    5

--- ESP-Project/main.py
+++ ESP-Project/main.py
@@ -44,7 +44,7 @@ try:
     # x = RFIDReader()
     gnd.off
     time.sleep(3)
-    # time.sleep(2) 4 5 6 7
+    # time.sleep(2) 4 5 6 7 8
     # x.scan()
     # test 2 permission
     # ic = connect_or_create_wifi()
